924d96d192082fdfae55fb49b1fda113e18ce25d
Birkin James Diana <birkin.diana@gmail.com>
2024-08-16 07:56:53 -0400

    adds comment to pymysql setup.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
+## not needed for local development
+## TODO: get mysqlclient to install on prod -- and remove all references to pymysql
+
 try:
     import pymysql
     pymysql.install_as_MySQLdb()
