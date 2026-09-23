de31e996999e929d1fd34eb309e78998187c15fc
Nicole Currens <nicole_currens@brown.edu>
2026-09-23 10:53:40 -0700

    rearrange a tiny bit

--- disa_app/static/js/entry_form_vue-item_mixin_save.js
+++ disa_app/static/js/entry_form_vue-item_mixin_save.js
@@ -404,6 +404,7 @@ async function saveItemDataToServer() {
       national_context: this.currentItem.national_context_id,
       citation_id: this.formData.doc.id,
       image_url: this.currentItem.kludge.image_url,
+      image_id: 1234,
       // Citation fields for the Record: compile into a hash between 
       //  Zotero field ID (without leading 'z') and value;
       //  Only add if not empty
@@ -411,8 +412,6 @@ async function saveItemDataToServer() {
         .filter(([k, v]) => v !== '')
         .reduce((acc, [k, v]) => { acc[k.substring(1)] = v; return acc }, {}),
       researcher_notes: this.currentItem.researcher_notes,
-      // TODO dynamically fill in this field from the form
-      image_id: 1234,
     };
 
     const httpMethod = isNewItem ? 'POST' : 'PUT';
